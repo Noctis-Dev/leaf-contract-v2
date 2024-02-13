7a41eed1c7d58a5ba557b1c7c6e58d5e174a2d8e
Carlos Chanona <carloseduardochanonaaquino@gmail.com>
2024-02-13 02:15:47 +0000

    search by name and description

--- state/src/lib.rs
+++ state/src/lib.rs
@@ -9,15 +9,27 @@ pub mod metafns {
     pub type State = LeafContractState;
     
     pub fn search(state: State, name: String, description: String) -> Event {
-        let projects = state.projects;
-        // (u128, Project)
-        let name_result: Vec<(u128, Project)> = projects.iter().filter(|&element| element.1.name == name).cloned().collect();
-        
-        if !name_result.is_empty() {
-            return Event::ProjectsFound { projects: name_result }
-        }
-        return Event::ProjectsNotFound { message: String::from("Not Project was found") }
+
+    let projects = state.projects;
+    let mut name_result: Vec<(u128, Project)> = Vec::new();
+    let mut description_result: Vec<(u128, Project)> = Vec::new();
+
+    let description_lower = description.to_lowercase();
+
+    if !name == "" {
+        name_result = projects.iter().filter(|&element| element.1.name == name).cloned().collect();
     }
+    if !description.is_empty() {
+        description_result = projects.iter().filter(|&element| element.1.description.to_lowercase().contains(&description_lower)).cloned().collect();
+    }
+
+    let projects_found = [name_result, description_result].concat();
+
+    if !projects_found.is_empty() {
+        return Event::ProjectsFound { projects: projects_found };
+    }
+    return Event::ProjectsNotFound { message: String::from("No project was found") };
+}
 
     pub fn get_by_id(state: State, project_id: u128) -> Event {
         let projects = state.projects;
