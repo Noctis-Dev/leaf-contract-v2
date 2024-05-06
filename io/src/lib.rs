d2c91d4d8d9f3634af74f959e0c629c08c6b81f1
JBunCE <sr.conejo.uwu@gmail.com>
2024-05-06 02:17:43 +0000

    leaf contract refactor, checkout workflow and code cleanup

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -1,16 +1,19 @@
 #![no_std]
+use escrow_io::{EscrowAction, EscrowEvent};
 use gstd::{ prelude::*, ActorId, CodeId };
 use gmeta::{In, InOut, Metadata, Out};
 
-pub type MilestoneId = u8;
+pub mod escrow_io;
 
-#[derive(Encode, Decode, TypeInfo, Hash, PartialEq, PartialOrd, Eq, Ord, Clone, Debug)]
+pub type MilestoneId = u8;
+pub type EscrowId = ActorId;
+
+#[derive(TypeInfo, Encode, Decode, Clone)]
 pub enum Action {
-    HelloAction,
     CreateProject {
         name: String,
         description: String,
-        milestone_value: Vec<(u128, MilestoneId)>,
+        milestones: Vec<Milestone>,
     },
     UpdateProject {
         id: ActorId,
@@ -28,7 +31,7 @@ pub enum Action {
     },
 }
 
-#[derive(Debug, Encode, Decode, TypeInfo)]
+#[derive(TypeInfo, Encode, Decode)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum  Event {
@@ -36,60 +39,32 @@ pub enum  Event {
     ProjectCreated {
         project: Project
     },
-    ProjectsFound {
-        projects: Vec<(ActorId, Project)>
-    },
     ProjectsNotFound {
         message: String
     },
-    ProjectFound {
-        project: (u128, Project)
-    }
-}
-
-#[derive(Debug, Encode, Decode, TypeInfo)]
-#[codec(crate = gstd::codec)]
-#[scale_info(crate = gstd::scale_info)]
-pub enum ScrowAction {
-    Deposit {
-        founder: ActorId
-    },
-    Aprove,
-    Reject,
-}
-
-#[derive(Debug, Encode, Decode, TypeInfo)]
-#[codec(crate = gstd::codec)]
-#[scale_info(crate = gstd::scale_info)]
-pub enum ScrowEvent {
-    DeposidedCompleted {
-        total_balance: u128,
-    },
-    AprovedCompleted,
-    RejectedCompleted,
     Error {
         message: String
     }
 }
 
-#[derive(TypeInfo, Encode, Decode, Default)]
-pub struct InitScrow {
-    pub collector: ActorId,
-    pub max_milestone_number: MilestoneId
+#[derive(TypeInfo, Encode, Decode, Clone)]
+pub struct Milestone {
+    pub title: String,
+    pub value: u128,
 }
 
-#[derive(Default, Decode, Clone, TypeInfo, Encode, Debug)]
+#[derive(Default, TypeInfo, Encode, Decode, Clone)]
 pub struct Project {
+    pub escrow_id: EscrowId,
     pub owner: ActorId,
     pub name: String,
     pub description: String,
-    pub scrow_id: ActorId,
-    pub milestone_value: Vec<(u128, MilestoneId)>,
+    pub milestone_value: Vec<Milestone>,
 }
 
-#[derive(Default, Decode, Clone, TypeInfo, Encode)]
+#[derive(Default, TypeInfo, Encode, Decode, Clone)]
 pub struct LeafContractState {
-    pub projects: Vec<(ActorId, Project)>
+    pub projects: Vec<Project>
 }
 
 pub struct ContractMetadata;
@@ -99,7 +74,7 @@ impl Metadata for ContractMetadata{
     type Init = In<CodeId>;
     type Handle = InOut<Action, Event>;
     type Others = ();
-    type Reply=();
+    type Reply = InOut<EscrowAction, EscrowEvent>;
     type Signal = ();
     type State = Out<LeafContractState>;
 }
--- src/handle.rs
+++ src/handle.rs
@@ -1,33 +1,30 @@
-use gstd::{ msg , prelude::*};
+use gstd::{ async_main, msg, prelude::*};
 use io::*;
 
 use crate::contract_mut;
 
-#[no_mangle]
-extern "C" fn handle(){
+#[async_main]
+async fn main(){
     let action: Action = msg::load().expect("Could not load Action");
     let source = msg::source();
 
     let contract = contract_mut();
 
     match &action {
-        Action::HelloAction => {
-            contract.say_hello()
-        }
-        Action::CreateProject { name, description, milestone_value } => {
-            contract.create_project(source, name.to_string(), description.to_string(), milestone_value.clone());
+        Action::CreateProject { name, description, milestones } => {
+            contract.create_project(source, name.to_string(), description.to_string(), milestones.clone()).await;
         }
         Action::UpdateProject { id, name, description } => {
-            contract.update_project(*id, name.to_string(), description.to_string());
+            contract.update_project(*id, name.to_string(), description.to_string()).await;
         }
         Action::Donate { scrow_id } => {
-            contract.donate(scrow_id.clone());
+            contract.donate(scrow_id.clone(), source).await;
         },
         Action::Aprove { scrow_id } => {
-            contract.aprove(scrow_id.clone());
+            contract.aprove(scrow_id.clone()).await;
         },
         Action::Reject { scrow_id } => {
-            contract.aprove(scrow_id.clone());
+            contract.aprove(scrow_id.clone()).await;
         },
     };
 }
